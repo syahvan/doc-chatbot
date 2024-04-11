8e4fdad8fc3013af6c660db659b179c973fe59bd
Syahvan <syahvanalviansyah@students.undip.ac.id>
2024-04-11 10:41:39 +0700

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# doc-chatbot
-Developed a Retrieval-Augmented Generation (RAG) multi-document chatbot application using Langchain, Streamlit, and Groq API
+# Multi-Documents ChatBot App"
+
+## Introduction
+
+The MultiDocument Chatbot App is a Python application that allows you to engage in conversations with multiple documents simultaneously. You can ask questions about the loaded documents using natural language, and the chatbot will provide relevant responses based on the content of the documents. This app utilizes a language model to generate accurate answers to your queries. Please note that the app will only respond to questions related to the loaded PDFs.
+
+## How It Works
+
+![MultiDocument Chat App Diagram](./img/process-diagram.png)
+
+The application follows these steps to provide responses to your questions:
+1. Document Loading: The app reads multiple documents and extracts their text content.
+2. Text Chunking: The extracted text is divided into smaller chunks that can be processed effectively.
+3. Language Model: The application utilizes a language model to generate vector representations (embeddings) of the text chunks.
+4. Similarity Matching: When you ask a question, the app compares it with the text chunks and identifies the most semantically similar ones.
+5. Response Generation: The selected chunks are passed to the language model, which generates a response based on the relevant content of the PDFs.
+
+## Dependencies and Installation
+
+To install the MultiPDF Chat App, please follow these steps:
+
+1. Clone the repository to your local machine.
+2. Install the required dependencies by running the following command:
+   ```
+   pip install -r requirements.txt
+   ```
+3. Obtain an API key from Groq and add it to the `.env` file in the project directory.
+```commandline
+GROQ_API_KEY=YOUR_API_TOKEN
+```
+
+## Usage
+-----
+To use the MultiDocument Chatbot App, follow these steps:
+
+1. Ensure that you have installed the required dependencies and added the Groq API key to the `.env` file.
+2. Run the `main.py` file using the Streamlit CLI. Execute the following command:
+   ```
+   streamlit run app.py
+   ```
+3. The application will launch in your default web browser, displaying the user interface.
+4. Load multiple documents into the app by following the provided instructions.
+5. Ask questions in natural language about the loaded PDFs using the chat interface.
---
+++ app.py
@@ -0,0 +1,127 @@
+import streamlit as st
+from streamlit_chat import message
+from langchain.chains import ConversationalRetrievalChain
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.memory import ConversationBufferMemory
+from langchain_core.prompts import PromptTemplate
+from langchain_community.vectorstores import FAISS
+from PyPDF2 import PdfReader
+import docx2txt
+from langchain_community.llms import Ollama
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+import tempfile
+
+load_dotenv()  
+groq_api_key = os.environ['GROQ_API_KEY']
+
+def initialize_session_state():
+    if 'history' not in st.session_state:
+        st.session_state['history'] = []
+
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = ["Hello! Ask me anything about 🤗"]
+
+    if 'past' not in st.session_state:
+        st.session_state['past'] = ["Hey! 👋"]
+
+
+def conversation_chat(query, chain, history):
+    template = """
+        Answer the question only based on the context below. 
+        If you can't answer the question based on the context below, reply "I don't know".
+
+        Context: {context}
+
+        Question: {question}
+    """
+    prompt = PromptTemplate.from_template(template)
+
+    result = chain.invoke({"question": query, "chat_history": history}, prompt=prompt)
+    history.append((query, result["answer"]))
+    return result["answer"]
+
+def display_chat_history(chain):
+    reply_container = st.container()
+    container = st.container()
+
+    with container:
+        user_input = st.chat_input("Ask me something....")
+
+        if user_input:
+            with st.spinner('Generating response...'):
+                output = conversation_chat(user_input, chain, st.session_state['history'])
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+
+    if st.session_state['generated']:
+        with reply_container:
+            for i in range(len(st.session_state['generated'])):
+                message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="avataaars", seed="Aneka")
+                message(st.session_state["generated"][i], key=str(i), avatar_style="bottts", seed="Aneka")
+
+
+def create_conversational_chain(vector_store):
+    # Create llm
+    llm = ChatGroq(
+            groq_api_key=groq_api_key, 
+            model_name='mixtral-8x7b-32768'
+    )
+
+    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+    chain = ConversationalRetrievalChain.from_llm(llm=llm, chain_type='stuff',
+                                                 retriever=vector_store.as_retriever(search_kwargs={"k": 2}),
+                                                 memory=memory)
+    return chain
+
+def main():
+    load_dotenv()  
+    groq_api_key = os.environ['GROQ_API_KEY']
+    # Initialize session state
+    initialize_session_state()
+    st.set_page_config(page_title="Ask your Document")
+    st.header("Ask your Document 💬")
+    st.markdown("a Multi-Documents ChatBot App")
+    # Initialize Streamlit
+    st.sidebar.title("Document Processing")
+    uploaded_files = st.sidebar.file_uploader("Upload your file here (.pdf, .docx, or .txt)", type=["pdf", "txt", "docx"], accept_multiple_files=True)
+    
+    # extract text from uploaded files
+    all_text = ""
+    for uploaded_file in uploaded_files:
+        if uploaded_file.type == "application/pdf":
+            pdf_reader = PdfReader(uploaded_file)
+            text = ""
+            for page in pdf_reader.pages:
+                text += page.extract_text()
+        elif uploaded_file.type == "text/plain":
+            text = uploaded_file.read().decode("utf-8")
+        elif uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            text = docx2txt.process(uploaded_file)
+        else:
+            st.write(f"Unsupported file type: {uploaded_file.name}")
+            continue
+        all_text += text
+
+    text_splitter = CharacterTextSplitter(separator="\n", chunk_size=1000, chunk_overlap=200, length_function=len)
+    text_chunks = text_splitter.split_text(all_text)
+
+    # Create embeddings
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+
+    if uploaded_files:
+        with st.spinner('Analyze Document...'):
+            # Create vector store
+            vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+
+        # Create the chain object
+        chain = create_conversational_chain(vector_store)
+                
+        display_chat_history(chain)
+
+if __name__ == "__main__":
+    main()
+
