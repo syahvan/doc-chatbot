fd6c2e128af06f577a28d525e1b6a68ea3aa7ec3
Syahvan <syahvanalviansyah@students.undip.ac.id>
2024-04-11 12:15:23 +0700

    use llama2

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def create_conversational_chain(vector_store):
     # Create llm
     llm = ChatGroq(
             groq_api_key=groq_api_key, 
-            model_name='mixtral-8x7b-32768'
+            model_name='llama2-70b-4096'
     )
 
     memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
